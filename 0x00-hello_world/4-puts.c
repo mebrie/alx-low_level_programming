fe06e97450a22640d601570b4bb87756d5fcd753
mebrie <mebrieka@gmail.com>
2022-04-07 12:06:48 -0700

    C puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 *main-entry point
 *
-*Return:0 after printing the function
+*Return: 0 afiter printing the function
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
