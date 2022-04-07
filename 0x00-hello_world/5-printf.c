34b325c0fbc453bd6361a14ae99d65f75c9b19d0
mebrie <mebrieka@gmail.com>
2022-04-07 12:23:45 -0700

    C printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
 *main-entry point
 *My hello, printf task
-*Return:1 after the program execution
+*Return: 1 after the program execution
 */
 int main(void)
 {
-puts("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
