d01598a8721b0d795a49172a51c9531e372706ae
mebrie <mebrieka@gmail.com>
2022-04-12 12:29:12 -0700

    Checks for an alphabetic character. Recreation of isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *_isalpha(int c)  checks for alphabetic character.
- *@Checks for an alphabetic character. Recreation of ``isalpha``
+ * _isalpha(int c)  checks for alphabetic character.
+ * @C: Checks for an alphabetic character. Recreation of ``isalpha``
  * Return: Always 0.
  */
 int _isalpha(int c)
