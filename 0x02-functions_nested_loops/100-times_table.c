ba3ebb15e039b0a119f3348198064b20a9d9b406
mebrie <mebrieka@gmail.com>
2022-04-12 14:14:49 -0700

    C 0x02-functions_nested_loops 3 to 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,49 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
-void times_table(int n)
+void print_times_table(int n)
 {
-int i, j, multi;
-if (!(n > 15 || n < 0))
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-multi = (i * j);
-if (j != 0)
-{
-putchar(',');
-putchar(' ');
-}
-if (multi < 10 && j != 0)
-{
-putchar(' ');
-putchar(' ');
-putchar((multi % 10) + '0');
-}
-else if (multi >= 10 && multi < 100)
-{
-putchar(' ');
-putchar((multi / 10) + '0');
-putchar((multi % 10) + '0');
-}
-else if (multi >= 100 && j != 0)
-{
-putchar((multi / 100) + '0');
-putchar((multi / 10) % 10 + '0');
-putchar((multi % 10) + '0');
-}
-else
-putchar((multi % 10) + '0');
-}
-putchar('\n');
-}
-}
+	int x = 0, y, z;
+
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
